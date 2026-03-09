741657b5e65f6e4433da19a5782cfa521f51e4c4
toby-gamez <henematobik213@gmail.com>
2026-03-09 19:04:17 +0100

    fix: Add missing import for SuppressLint annotation in MainActivity

--- composeApp/src/androidMain/kotlin/com/tkolymp/tkolympapp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/tkolymp/tkolympapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.tkolymp.tkolympapp
 
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
