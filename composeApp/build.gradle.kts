e31e5f6fcdc555cca63c32afffec40cb975b5999
toby-gamez <henematobik213@gmail.com>
2026-03-13 22:46:33 +0100

    chore: Update versionName to 1.1 in build.gradle.kts

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -50,7 +50,7 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.1"
     }
     packaging {
         resources {
