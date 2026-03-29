37a9b91db024c82cbd2fea5c41b1d56881261341
toby-gamez <henematobik213@gmail.com>
2026-03-29 17:59:17 +0200

    chore: Add wrapContentWidth and FontWeight imports in LeaderboardScreen for layout enhancements

--- composeApp/src/commonMain/kotlin/com/tkolymp/tkolympapp/screens/LeaderboardScreen.kt
+++ composeApp/src/commonMain/kotlin/com/tkolymp/tkolympapp/screens/LeaderboardScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -32,6 +33,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
